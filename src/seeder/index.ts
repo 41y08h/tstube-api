ec1e64a83e90ba7e2f669a9a85aa48a790233188
Piyush <piyush41y08h@gmail.com>
2021-07-29 22:00:14 +0530

    Add seeder script

---
+++ src/seeder/index.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import db from "../db";
+import createDebug from "debug";
+import videosData from "./videos.data";
+import clearConsole from "../lib/clearConsole";
+
+async function main() {
+  const debug = createDebug("app:seeder");
+
+  const videosSeeds = videosData.map(async (videoData) => {
+    const {
+      rows: [video],
+    } = await db.query(
+      `
+    insert into "Video"
+    (title, description, thumbnail, src, duration, "userId")
+    values ($1, $2, $3, $4, $5, $6)
+    returning *
+    `,
+      [
+        videoData.title,
+        videoData.description,
+        videoData.thumbnail,
+        videoData.src,
+        videoData.duration,
+        1,
+      ]
+    );
+    debug(`🌱 Added video ${video.title}`);
+  });
+
+  await Promise.all(videosSeeds);
+}
+
+clearConsole();
+main().then(() => process.exit());
