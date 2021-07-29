f311a6c92963c43e880fdc384d53dc24d9396a01
Piyush <piyush41y08h@gmail.com>
2021-07-29 22:22:23 +0530

    Paginate videos list api

--- src/controllers/Videos/getAll.ts
+++ src/controllers/Videos/getAll.ts
@@ -2,8 +2,29 @@ import db from "../../db";
 import handler from "../../lib/handler";
 
 const getAll = handler(async (req, res) => {
-  const { rows: videos } = await db.query(`select * from "Video"`);
-  res.json(videos);
+  const page = parseInt(req.query.page as string) || 1;
+
+  const { rows: videos } = await db.query(
+    `
+    select * from "Video" 
+    order by "createdAt" desc
+    offset ($1 - 1) * 24
+    limit 24
+    `,
+    [page]
+  );
+
+  const {
+    rows: [{ total }],
+  } = await db.query(
+    `select cast(count("Video") as int) as total from "Video"`
+  );
+
+  res.json({
+    page,
+    hasMore: page * 24 < total,
+    items: videos,
+  });
 });
 
 export default getAll;
